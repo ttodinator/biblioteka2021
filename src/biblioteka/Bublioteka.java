d091374223d82ccae3e1303ad5e77f90223cadcc
ttodinator <ttodinator@gmail.com>
2021-03-08 12:42:41 +0100

    Napravljena klasa Biblioteka koja implementira intefejs

---
+++ src/biblioteka/Bublioteka.java
@@ -0,0 +1,33 @@
+package biblioteka;
+
+import java.util.LinkedList;
+
+import biblioteka.interfejs.BibliotekaInterface;
+
+public class Bublioteka implements BibliotekaInterface {
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public LinkedList<Knjiga> vratiSveKnjige() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String izdavac) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
